e755f2718ce293a0dfe6ddaac124a7de5572be6e
Charlie Yan <charlieyan08@gmail.com>
2014-10-16 01:53:53 -0700

    working storeCredit solution

--- gcj/storeCredit.py
+++ gcj/storeCredit.py
@@ -1,6 +1,39 @@
 #https://code.google.com/codejam/contest/351101/dashboard#s=p0
 
-#subset sum problem
-f = open('storeCredit_small.in','r')
+def solve(arr, credit):
+  #returns a tuple of 2 indexes (base 0)
+  d = dict();
+  for i in range(len(arr)):
+    k = credit-arr[i]
+    if k in d.keys():
+      d[k].append(i)
+    else:
+      d[k] = [i]
+  for i in range(len(arr)):
+    if arr[i] in d.keys():
+      for j in d[arr[i]]:
+        if (j != i):
+          return (i+1,j+1)
+
+#f = open('storeCredit_small.in','r')
+f = open('A-large-practice.in','r')
+# f = open('A-large-practice.in','r')
+N = f.readline().replace("\n","")
+STATECREDIT = 0
+STATEITEMC = 1
+STATEITEMS = 2
+count = 0
+currentCredit = None
+currentCaseNum = 1
 for line in f:
-  print line
+  s = line.replace("\n","")
+  if (count is STATECREDIT):
+    currentCredit = int(s)
+  elif (count is STATEITEMS):
+    arr = [int(st) for st in s.split(" ")]
+    r = solve(arr,currentCredit)
+    f = min(r[0],r[1])
+    l = max(r[0],r[1])
+    print "Case #" + str(currentCaseNum) + ": " + str(f) + " " + str(l)
+    currentCaseNum = currentCaseNum + 1
+  count = (count + 1) % 3
