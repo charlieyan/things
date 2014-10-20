abb5c837d99b0405d77b009ac305f1ce79d0df95
Charlie Yan <charlieyan08@gmail.com>
2014-10-20 00:40:55 -0400

    reverseWords solution

--- gcj/reverseWords.py
+++ gcj/reverseWords.py
@@ -1,4 +1,12 @@
 def reverseWord(s):
   # reverse a string with words by word in O(n) time
-  words = s.split(" ")
+  return " ".join(s.split(" ")[::-1])
 
+fileName = 'reverseS.in'
+fo = open(fileName,'r')
+num = fo.readline()
+n = 0
+for line in fo:
+  n = n + 1
+  ans = reverseWord(line.replace("\n",""))
+  print "Case #" + str(n) + ": " + ans
