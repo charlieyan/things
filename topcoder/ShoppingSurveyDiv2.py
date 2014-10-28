b79f0cbef24e50a978aa596c1fe9932ebb65f20d
Charlie Yan <charlieyan08@gmail.com>
2014-10-27 23:56:31 -0700

    start of shopping survey question

---
+++ topcoder/ShoppingSurveyDiv2.py
@@ -0,0 +1,9 @@
+class ShoppingSurveyDiv2:
+  def minValue(self, N, s):
+    # infer total number of items in the store as sum(s)
+
+    # 
+    return sum(s)
+
+s = ShoppingSurveyDiv2()
+print s.minValue(5, (3, 3))
