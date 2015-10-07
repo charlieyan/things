ebd9ef55803e4d643421b383e595686ef94f02cc
Charlie Yan <charlieyan08@gmail.com>
2015-10-06 17:16:05 -0700

    latest

---
+++ leetcode/BinaryTreePaths.java
@@ -0,0 +1,16 @@
+class TreeNode {
+   	int val;
+	TreeNode left;
+   	TreeNode right;
+   	TreeNode(int x) { val = x; }
+}
+
+public class BinaryTreePaths {
+    public List<String> binaryTreePaths(TreeNode root) {
+        
+    }
+
+	public static void main(String[] args) {
+
+	}
+}
